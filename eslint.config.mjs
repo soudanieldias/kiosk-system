e82ec164c38ab298cfbb49ec750ed4e630e9eb81
soudanieldias <soudanieldias@hotmail.com>
2026-06-12 20:44:17 -0300

    chore: add some project settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,31 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+
+  {
+    name: "project/import-sorting",
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
